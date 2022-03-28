2eec3bb251a75ce2f316d0bacf2ba2318503220b
scholarbee <scholarbee6@gmail.com>
2022-03-28 02:55:40 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+require('./models/db');
+
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const bodyparser = require('body-parser');
+
+const employeeController = require('./controllers/employeeController');
+
+var app = express();
+
+var port = process.env.PORT || 3000
+
+app.use(bodyparser.urlencoded({
+    extended: true
+}));
+app.use(bodyparser.json());
+app.set('views', path.join(__dirname, '/views/'));
+app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/' }));
+app.set('view engine', 'hbs');
+
+
+app.use('/', employeeController);
+// app.use('/employee', employeeController);
+
+app.listen(port, () => {
+    console.log(`Express server started at http://localhost:${port}`);
+});
